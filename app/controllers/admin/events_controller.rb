9437a3841d6eff64776980baff97a65c0fd12ea2
manishh94 <manishprajapat94@gmail.com>
2018-05-15 11:43:59 +0530

    Updated events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,7 +4,7 @@ module Admin
     layout 'theme'
 
     def index
-      @events = Event.all
+      @events = Event.all.order(created_at: :desc)
     end
 
     def new
@@ -13,8 +13,8 @@ module Admin
 
     def create
       @event = Event.new(admin_event_attributes)
-      start_date = Date.parse(params[:event][:start_date].to_s)
-      end_date = Date.parse(params[:event][:end_date].to_s)
+      start_date = Date.parse(params[:event][:start_date].to_s) if params[:event][:start_date].present?
+      end_date = Date.parse(params[:event][:end_date].to_s) if params[:event][:end_date].present?
       @event.assign_attributes(start_date: start_date, end_date: end_date, user_id: current_user.id, public: true)
       respond_to do |format|
         if @event.save
@@ -29,12 +29,21 @@ module Admin
     end
 
     def update
+      start_date = Date.parse(params[:event][:start_date].to_s) if params[:event][:start_date].present?
+      end_date = Date.parse(params[:event][:end_date].to_s) if params[:event][:end_date].present?
+      @event.assign_attributes(admin_event_attributes.merge(start_date: start_date, end_date: end_date, user_id: current_user.id, public: true))
       if @event.update(admin_event_attributes)
         flash[:notice] = 'event updated successfully.'
       else
         flash[:error] = 'Failed to update event.'
       end
-      redirect_to admin_events_path
+      respond_to do |format|
+        if @event.save
+          format.html { redirect_to admin_events_path, notice: 'Event was successfully created.' }
+        else
+          format.html { render :new }
+        end
+      end
     end
 
     private
